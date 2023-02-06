e26c668154a29eb5fe9bbd2ea25c6d672f0331f5
ClausHofman <46158@edu.raseko.fi>
2023-02-06 09:06:45 +0200

    auto-luokka

---
+++ 30-01/iban_tarkastaja.py
@@ -0,0 +1,15 @@
+from stdnum import iban
+
+def main():
+    tilinumero = input("Anna IBAN-tilinumero: ")
+    on_oikea_iban = iban.is_valid(tilinumero)
+    if on_oikea_iban:
+        print(f"Antamasi tilinumero {tilinumero} on oikea IBAN.")
+        muotoiltu = iban.format(tilinumero)
+        print(f"Tilinumero muotoiltuna: {muotoiltu}")
+    else:
+        print(f"Antamasi tilinumero '{tilinumero}' ei ole oikea IBAN.")
+
+
+if __name__ == '__main__':
+    main()
